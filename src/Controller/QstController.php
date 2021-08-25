cc54055f42bb9989e58792a7d1ac8729c19a2597
Mozaritta <mozaritta@gmail.com>
2021-08-25 11:58:02 +0100

    Add create questions' form

--- src/Controller/QstController.php
+++ src/Controller/QstController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Questions;
+use App\Form\QuestionsType;
 use App\Repository\QuestionsRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,5 +21,28 @@ class QstController extends AbstractController
     {
         $qsts = $qstRepository->findBy([], ['createdAt' => 'DESC']);
         return $this->render('qst/index.html.twig', compact('qsts'));
+    }
+
+    /**
+     * @Route("/add", name="add_qst", methods="GET|POST")
+     */
+    public function addQst(Request $request, EntityManagerInterface $em): Response
+    {
+        $question = new Questions;
+        $form = $this->createForm(QuestionsType::class, $question);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($question);
+            $em->flush();
+            $this->addFlash(
+                'primary',
+                'Question added successfully'
+            );
+            return $this->redirectToRoute('app_home');
+        }
+        return $this->render('qst/add.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
---
+++ src/Form/QuestionsType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Questions;
+use App\Entity\Type;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class QuestionsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        // $typeRepository = new TypeRepository;
+        // $type = $typeRepository->findBy([]);
+        $builder
+            ->add('description')
+            ->add('valid', CheckboxType::class, ['value' => false, 'disabled' => true]) // disabled fot the client but should be enabled for admins
+            ->add('type', EntityType::class, [
+                'class' => Type::class,
+                'choice_label' => 'name'
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Questions::class,
+        ]);
+    }
+}
